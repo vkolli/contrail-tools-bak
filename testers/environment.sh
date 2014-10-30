4cbe6e1e38e9b98c89f5c0c77c384aed59a1819a
Vedu Joshi <vjoshi@juniper.net>
2014-10-30 14:22:54 +0530

    Add help on environment variables

--- testers/environment.sh
+++ testers/environment.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 export TBFILE
 export API_SERVER_HOST_STRING=${API_SERVER_HOST_STRING:-"root@127.0.0.1"}
+export TOOLS_WS=${TOOLS_WS:-$PWD}
+
+# Set the below 4 variables if PKG_FILE is not set
 export BRANCH=${BRANCH:-mainline}
 export BUILDID=${BUILDID:-LATEST}
 export DISTRO=${DISTRO:-"ubuntu-12-04"}
 export SKU=${SKU:-icehouse}
-export TOOLS_WS=${TOOLS_WS:-$PWD}
 
 # If BRANCH, BUILID, DISTRO, SKU are not defined,
 # PKG_FILE path needs to be set
@@ -13,6 +15,7 @@ export PKG_FILE
 
 #--------
 # For Tempest
+# You can override these from the results of 'fab export_testbed_details' task
 export KEYSTONE_SERVICE_HOST=${KEYSTONE_SERVICE_HOST:-127.0.0.1}
 export KEYSTONE_SERVICE_HOST_USER=${KEYSTONE_SERVICE_HOST_USER:-root}
 export KEYSTONE_SERVICE_HOST_PASSWORD=${KEYSTONE_SERVICE_HOST_PASSWORD:-c0ntrail123}
@@ -24,13 +27,24 @@ export API_SERVER_HOST_PASSWORD=${API_SERVER_HOST_PASSWORD:-c0ntrail123}
 export SKIP_REIMAGE=${SKIP_REIMAGE:-0}
 export SKIP_BRINGUP=${SKIP_BRINGUP:-0}
 export SKIP_SANITY=${SKIP_SANITY:-0}
+
+# Skip running tempest by default
 export SKIP_TEMPEST=${SKIP_TEMPEST:-1}
 
+# Set NEW_TEST_INFRA to 1 to use the contrail-test code from bhushana/contrail-test
 export NEW_TEST_INFRA=${NEW_TEST_INFRA:-0}
 export MX_GW_TEST=${MX_GW_TEST:-0}
+
+# By default, new test infra enables parallel run. To force it to run serially  
 export SERIAL_RUN=${SERIAL_RUN:-0}
+
+# Knob to add basic images (fab add_basic_images) before the test run
 export ADD_IMAGES=${ADD_IMAGES:-0}
+
+# Location can be US or INDIA
+# If US, reimage command will be triggered from SVL_HOST_STRING
 export TESTBED_LOCATION=${TESTBED_LOCATION:-INDIA}
+
 export SCRIPT_TIMESTAMP=`date +"%Y_%m_%d_%H_%M_%S"`
 export SSHOPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "
 export NODEHOME=${NODEHOME:-/root}
@@ -39,12 +53,15 @@ export SVL_HOST_PASSWORD=${SVL_HOST_PASSWORD:-c0ntrail123}
 export FAB_GIT_BRANCH=${BRANCH:-master}
 export PARAMS_FILE=${PARAMS_FILE:-${NODEHOME}/contrail-test/scripts/sanity_params.ini}
 export TEST_CONFIG_FILE=${TEST_CONFIG_FILE:-${NODEHOME}/contrail-test/sanity_params.ini}
+
 export BUILD_SCRIPT_PATH=$TOOLS_WS/contrail-test
 export FABRIC_SCRIPT_PATH='$TOOLS_WS/contrail-fabric-utils'
 export RUN_WITHIN_VENV=0
-export SANITY_TEST_TYPE="serial"
 export EMAIL_SUBJECT=${EMAIL_SUBJECT:-"Sanity Report"}
+
+# Location where locks are maintained for testbeds
 export LOCK_FILE_DIR=${LOCK_FILE_DIR:-/cs-shared/test_runs}
+
 if [ ${BRANCH} == "mainline" ]; then
     FAB_GIT_BRANCH='master'
 fi
@@ -61,11 +78,10 @@ lsb_dist=''
 # c) CFGM_FAB_PATH and CFGM_TEST_CODE_PATH are updated
 # d) JUNIPER_INTRANET is set to 0
 #
-# The default paths are ~/fabric-utils and ~/contrail-test on config node
+# The default path for fab code is ~/fabric-utils
 
 export CFGM_FAB_PATH=${CFGM_FAB_PATH:-${NODEHOME}/fabric-utils}
 export CFGM_TEST_CODE_PATH=${CFGM_TEST_CODE_PATH:-${NODEHOME}/contrail-test}
 export JUNIPER_INTRANET=${JUNIPER_INTRANET:-1}
 
-###########
 
