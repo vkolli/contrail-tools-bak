4989d3d9536fbeeeaa242e3db60cdbb7e4db89a4
Ankit Jain <ankitja@juniper.net>
2014-11-20 03:25:59 -0800

    Add run_ui_sanity.sh and modify utils

--- testers/reserve_and_run.sh
+++ testers/reserve_and_run.sh
@@ -4,36 +4,4 @@ TOOLS_WS=${TOOLS_WS:-$(pwd)}
 
 source $TOOLS_WS/testers/utils
 
-# AVAILABLE_TESTBEDS is a comma separated list of testbed filenames or paths
-testbeds=(${AVAILABLE_TESTBEDS//,/ })
-echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
-
-test_ran=0
-while :
-do
-    for i in "${testbeds[@]}"
-    do
-        tb_filename=`basename $i`
-        if lock_testbed $tb_filename ; then
-            trap cleanup EXIT
-            export TBFILE=$i
-            export TBFILE_NAME=`basename $TBFILE`
-            echo "Locked testbed $tb_filename"
-            run_task $tb_filename
-            unlock_testbed $tb_filename
-            test_ran=1
-            break
-        else
-            echo "Testbed $tb_filename is not yet available"
-            continue
-        fi
-    done
-    if [[ $test_ran -eq 1 ]]
-    then
-        echo "Test done"
-        break
-    fi
-    echo "Waiting for testbeds..retrying in 60 sec"
-    sleep 60
-done
-    
+run_task
---
+++ testers/run_ui_sanity.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+TOOLS_WS=${TOOLS_WS:-$(pwd)}
+
+source $TOOLS_WS/testers/utils
+
+
+function run_ui_sanity {
+    if [ $SKIP_SANITY -ne 0 ]
+    then
+        return 0
+    fi
+    search_package
+    copy_fabric_test_artifacts
+    run_fab "setup_test_env"
+    run_fab "install_webui_packages:~"
+    check_venv_exists
+    setup_sanity_base
+}
+
+function run_ui_task() {
+    get_testbed
+    echo "running on testbed $TBFILE_NAME"
+    create_testbed || die "Failed to create required testbed details"
+    reimage_and_bringup
+    #bringup_setup || die "Bringup failed"
+    run_ui_sanity || die "Run_sanity step failed"
+    unlock_testbed $TBFILE_NAME
+    echo "Test Done" 
+    collect_tech_support || die "Task to collect logs/cores failed"
+    echo "Ending test on $TBFILE_NAME"
+}
+
+run_ui_task 
