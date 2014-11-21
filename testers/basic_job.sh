bdd28e9e8d1d9b14c2edc84c44a60531ca743606
Ankit Jain <ankitja@juniper.net>
2014-11-21 02:54:08 -0800

    Removed create_testbed

--- testers/basic_job.sh
+++ testers/basic_job.sh
@@ -7,6 +7,5 @@ testbeds=(${AVAILABLE_TESTBEDS//,/ })
 echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
 
 get_testbed
-create_testbed || die "Failed to create required testbed details"
 run_task
 unlock_testbed $TBFILE_NAME
