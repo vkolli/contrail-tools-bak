7af02c4e12102300a86350f7745c37864fc635d4
Bharat <pbharat@juniper.net>
2015-01-01 21:25:03 -0800

    corrected contrail-test path in testbed file

--- testbeds/testbed_smgr_multi_node.py
+++ testbeds/testbed_smgr_multi_node.py
@@ -78,7 +78,7 @@ multi_tenancy = True
 #To Enable prallel execution of task in multiple nodes
 #do_parallel = True
 #haproxy = True
-env.test_repo_dir='/home/stack/sudhee/19Nov/contrail-test'
+env.test_repo_dir='/home/stack/smgr_github_ubuntu_multi_node/contrail-test'
 env.mail_to='dl-contrail-sw@juniper.net'
 env.log_scenario='Server Manager Multi-Node Sanity'
 
