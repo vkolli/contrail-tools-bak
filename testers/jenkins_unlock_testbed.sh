910a06e8a7d21d4462dad76fd061045f671bb071
Vedu Joshi <vjoshi@juniper.net>
2015-03-01 16:14:08 +0530

    Update jenkins_unlock_testbed.sh as per commit f2541d5e
    In f2541d5e, while locking, we had introducted additional info
    like buildid, release etc.
    jenkins_unlock_testbed.sh should also be mindful of that
    Updated the same

--- testers/jenkins_unlock_testbed.sh
+++ testers/jenkins_unlock_testbed.sh
@@ -5,13 +5,13 @@ echo "Current active jobs: "
 
 (
   flock 5
-  tb_lock_file=`grep -l "Occupied! ($BUILD_TAG)" $LOCK_FILE_DIR/*.py` || \
+  tb_lock_file=`grep -l "Occupied! .*$BUILD_TAG" $LOCK_FILE_DIR/*.py` || \
                 echo "No testbed found for unlocking"
   if [[ ! -s $tb_lock_file ]]; then
     exit 0
   fi
   if ! grep -xq "Testbed locked..Unlock when debug complete" $tb_lock_file ; then
-    sed -i "/Occupied! ($BUILD_TAG)/d" $tb_lock_file
+    sed -i "/Occupied! .*$BUILD_TAG/d" $tb_lock_file
   else
     echo "Testbed $tb_lock_file is locked for debug..."
   fi
