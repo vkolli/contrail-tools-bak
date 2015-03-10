d34551b7f9ccf280552bdecce1077b1dc21faa09
Sandip Dey <sandipd@juniper.net>
2015-03-09 23:31:08 -0700

    Changing testbed

--- testbeds/nodea11_a29.py
+++ testbeds/nodea11_a29.py
@@ -5,7 +5,6 @@ host2 = 'root@10.204.216.14'
 host3 = 'root@10.204.216.15'
 host4 = 'root@10.204.216.25'
 host5 = 'root@10.204.217.75'
-host6 = 'root@10.204.217.4'
 
 ext_routers = [('blr-mx1', '10.204.216.253')]
 router_asn = 64512
@@ -16,29 +15,29 @@ host_build = 'sandipd@10.204.216.4'
 
 
 env.roledefs = {
-    'all': [host1, host2, host3,host4, host5,host6],
+    'all': [host1, host2, host3,host4, host5],
     'cfgm': [host1,host2,host3],
     'webui': [host1],
     'openstack': [host1],
     'control': [host2, host3],
     'collector': [host1,host2],
     'database': [host1],
-    'compute': [host4, host5, host6],
+    'compute': [host4, host5],
     'build': [host_build]
 }
 
 env.hostnames = {
-    'all': ['nodea11', 'nodea18', 'nodea19', 'nodea29', 'nodeg35', 'nodec19']
+    'all': ['nodea11', 'nodea18', 'nodea19', 'nodea29', 'nodeg35']
 }
 #env.interface_rename = False
 #
-#control_data = {
-#    host1 : { 'ip': '192.168.193.1/24', 'gw' : '192.168.193.254', 'device':'eth3' },
-#    host2 : { 'ip': '192.168.193.2/24', 'gw' : '192.168.193.254', 'device':'eth3' },
-#    host3 : { 'ip': '192.168.193.3/24', 'gw' : '192.168.193.254', 'device':'eth3' },
-#    host4 : { 'ip': '192.168.193.4/24', 'gw' : '192.168.193.254', 'device':'eth3' },
-#    host5 : { 'ip': '192.168.193.5/24', 'gw' : '192.168.193.254', 'device':'eth3' },
-#}
+control_data = {
+    host1 : { 'ip': '192.168.250.1/24', 'gw' : '192.168.250.254', 'device':'eth0' },
+    host2 : { 'ip': '192.168.250.2/24', 'gw' : '192.168.250.254', 'device':'eth0' },
+    host3 : { 'ip': '192.168.250.3/24', 'gw' : '192.168.250.254', 'device':'eth0' },
+    host4 : { 'ip': '192.168.250.4/24', 'gw' : '192.168.250.254', 'device':'eth0' },
+    host5 : { 'ip': '192.168.251.5/24', 'gw' : '192.168.251.254', 'device':'eth1' },
+}
 
 env.openstack_admin_password = 'contrail123'
 env.password = 'c0ntrail123'
@@ -48,17 +47,15 @@ env.passwords = {
     host3: 'c0ntrail123',
     host4: 'c0ntrail123',
     host5: 'c0ntrail123',
-    host6: 'c0ntrail123',
     host_build: 'stack@123',
 }
 
 env.ostypes = {
-    host1:'centos',
-    host2:'centos',
-    host3:'centos',
-    host4:'centos',
-    host5:'centos',
-    host6:'centos',
+    host1:'ubuntu',
+    host2:'ubuntu',
+    host3:'ubuntu',
+    host4:'ubuntu',
+    host5:'ubuntu',
 }
 env.test_repo_dir='/home/stack/multi_interface_parallel/ubuntu/icehouse/contrail-test'
 env.mail_from='contrail-build@juniper.net'
