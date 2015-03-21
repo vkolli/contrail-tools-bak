ea9a3b08ea6059cf143d1039d1f1572aadc66836
root <root@cmbu-auto02.englab.juniper.net>
2015-03-21 09:28:26 -0700

    SM ceph sanity scripts

---
+++ testers/smgr_sanity_ceph_job.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+TOOLS_WS=${TOOLS_WS:-$(pwd)}
+source $TOOLS_WS/testers/utils
+source $TOOLS_WS/testers/utils_ceph
+source $TOOLS_WS/testers/smgr_utils
+source $TOOLS_WS/testers/smgr_utils_ceph
+# AVAILABLE_TESTBEDS is a comma separated list of testbed filenames or paths
+testbeds=(${AVAILABLE_TESTBEDS//,/ })
+echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
+
+get_testbed
+run_smgr_task_ceph
+unlock_testbed $TBFILE_NAME
