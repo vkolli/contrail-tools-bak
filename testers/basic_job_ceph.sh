5452ea469233f9ba17616136e9664f489b5a2b98
root <root@cmbu-auto02.englab.juniper.net>
2015-04-06 04:02:02 -0700

    storage script

---
+++ testers/basic_job_ceph.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TOOLS_WS=${TOOLS_WS:-$(pwd)}
+source $TOOLS_WS/testers/utils
+# AVAILABLE_TESTBEDS is a comma separated list of testbed filenames or paths
+testbeds=(${AVAILABLE_TESTBEDS//,/ })
+echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
+
+get_testbed
+run_task_ceph
+unlock_testbed $TBFILE_NAME
