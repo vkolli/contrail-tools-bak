ef856583ec9dce7161e5997de6f0451cd8b0795f
Vageesan Thanikachalam <vageesant@juniper.net>
2015-04-14 03:29:19 -0700

    print testcase name

--- testers/storage_scripts/execute_ceph_suite.py
+++ testers/storage_scripts/execute_ceph_suite.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 import commands
-commands.getoutput('python live_migrate.py live_migrate.conf SMOKE_CEPH3.config')
+print "Executing script : %s"%live_migrate.py
+print commands.getoutput('python live_migrate.py live_migrate.conf SMOKE_CEPH3.config')
 print "Executed ceph sanity successfully"
