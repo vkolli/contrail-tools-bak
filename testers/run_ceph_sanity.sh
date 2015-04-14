a56e93913be0681d131bfde430d8b046561b63c2
Vageesan Thanikachalam <vageesant@juniper.net>
2015-04-14 03:58:25 -0700

    added code to execute from TASKRUNNER host

--- testers/run_ceph_sanity.sh
+++ testers/run_ceph_sanity.sh
@@ -6,10 +6,11 @@ source $TOOLS_WS/testers/utils
 testbeds=(${AVAILABLE_TESTBEDS//,/ })
 echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
 
-get_testbed
-
-cd $TOOLS_WS
-cd testers/storage_scripts
-python execute_ceph_suite.py $AVAILABLE_TESTBEDS $PROFILE
-
-unlock_testbed $TBFILE_NAME
+#get_testbed
+exec_cmds -s $TASK_RUNNER_HOST_STRING -p $TASK_RUNNER_HOST_PASSWORD -c "
+    cd /tmp/$AVAILABLE_TESTBEDS/;
+    git clone git@github.com:Juniper/contrail-tools.git;
+    cd contrail-tools/testers/storage_scripts;
+    python execute_ceph_suite.py $AVAILABLE_TESTBEDS $PROFILE;
+  "
+#unlock_testbed $TBFILE_NAME
