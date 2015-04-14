ee35fb8e88b4966895163e8488b6116a3f58659f
Vageesan Thanikachalam <vageesant@juniper.net>
2015-04-13 22:50:14 -0700

    included testbed detail

--- testers/run_ceph_sanity.sh
+++ testers/run_ceph_sanity.sh
@@ -3,8 +3,13 @@
 TOOLS_WS=${TOOLS_WS:-$(pwd)}
 
 source $TOOLS_WS/testers/utils
+testbeds=(${AVAILABLE_TESTBEDS//,/ })
+echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
+
 get_testbed
 
 cd $TOOLS_WS
 cd storage_scripts
 python execute_ceph_suite.py $AVAILABLE_TESTBEDS $PROFILE
+
+unlock_testbed $TBFILE_NAME
