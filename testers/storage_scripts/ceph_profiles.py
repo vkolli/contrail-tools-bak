c753d5fb8edc961ecc5558eadda56d73291770db
Vageesan Thanikachalam <vageesant@juniper.net>
2015-05-07 23:07:04 -0700

    added testcase for PROF01

--- testers/storage_scripts/ceph_profiles.py
+++ testers/storage_scripts/ceph_profiles.py
@@ -1,5 +1,9 @@
 
 profiles_tests = {}
+profiles_tests['PROF01'] = []
+profiles_tests['PROF01'].append('python,check_cinder_type.py,check_cinder_type.conf')
+profiles_tests['PROF01'].append('python,live_migrate.py,live_migrate.conf')
+
 profiles_tests['PROF02'] = []
 profiles_tests['PROF02'].append('python,check_cinder_type.py,check_cinder_type.conf')
 profiles_tests['PROF02'].append('python,live_migrate.py,live_migrate.conf')
