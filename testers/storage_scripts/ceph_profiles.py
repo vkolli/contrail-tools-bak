cade94cdc6a35b329c0f9517f5e6f27342f8ce65
Vageesan Thanikachalam <vageesant@juniper.net>
2015-05-13 05:43:12 -0700

    add storage test added

--- testers/storage_scripts/ceph_profiles.py
+++ testers/storage_scripts/ceph_profiles.py
@@ -14,3 +14,7 @@ profiles_tests['PROF04'] = []
 profiles_tests['PROF04'].append('python,fix_ntp.py,fix_ntp.conf')
 profiles_tests['PROF04'].append('python,check_cinder_type.py,check_cinder_type.conf')
 #profiles_tests['PROF04'].append('python,live_migrate.py,live_migrate.conf')
+
+profiles_tests['PROF07'] = []
+profiles_tests['PROF07'].append('python,fix_ntp.py,fix_ntp.conf')
+profiles_tests['PROF07'].append('python,check_cinder_type.py,check_cinder_type.conf')
