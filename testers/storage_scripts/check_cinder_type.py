de08910c9a54b953884bfdee7c20dbecc1b32813
Vageesan Thanikachalam <vageesant@juniper.net>
2015-05-13 20:30:54 -0700

    logic added for local-only disks or nfs-only disks

--- testers/storage_scripts/check_cinder_type.py
+++ testers/storage_scripts/check_cinder_type.py
@@ -30,49 +30,60 @@ if __name__ == '__main__' :
 
     output = gen_lib.send_cmd(test_obj,fab_node_handle,"ceph -s","#",120)
     print output
-    ret = re.search('HEALTH_WARN|HEALTH_OK|HEALTH_ERR',output)
-    if ret and ret.group(0) in ['HEALTH_WARN','HEALTH_OK'] :
-       print "CEPH health status is OK"
-    else:
-       print "CEPH health status is NOT OK"
-       test_obj.argument['err_msg'] += "CEPH health status is NOT OK"
-       sys.exit(1)
-
-    if re.search('mons down',output):
-       msg = "ERROR: some mons process is down."
-       print msg
-       test_obj.argument['err_msg'] += msg
-       sys.exit(1)
-       
-    if re.search('stuck|degraded|unclean|undersized',output):
-       msg = "ERROR: ceph status has issue."
-       print msg
-       test_obj.argument['err_msg'] += msg
-       sys.exit(1)
-       
-    out = re.search('mons at.*',output).group()
-    ret = re.findall('=(.*?):',out)
-    ret.sort()
-    expected_mon_list = testbed_config['%s,mon_ip'%profile_name]
-    expected_mon_list.sort()
-    print ret,expected_mon_list
-    if ret == expected_mon_list :
-        print "INFO: mon IP is correct"
-    else:
-        print "ERROR: mon IP list is not correct"
-        test_obj.argument['err_msg'] += "ERROR: mon IP list is not correct"
+    if testbed_config['PROF05,osd_count'] == -1 :
+      if re.search('Error initializing cluster client: Error',output):
+        msg = "PASS : ceph status is correct.Error seen due to local-only disks or nfs-only disks"
+        print msg
+        sys.exit(0)
+      else:
+        msg = "ERROR : ceph status is incorrect.supposed to see \"Error initializing cluster client: Error\""
+        print msg
+        test_obj.argument['err_msg'] += msg
         sys.exit(1)
+    else:
+       ret = re.search('HEALTH_WARN|HEALTH_OK|HEALTH_ERR',output)
+       if ret and ret.group(0) in ['HEALTH_WARN','HEALTH_OK'] :
+          print "CEPH health status is OK"
+       else:
+          print "CEPH health status is NOT OK"
+          test_obj.argument['err_msg'] += "CEPH health status is NOT OK"
+          sys.exit(1)
 
-    exp_osd_map_count = testbed_config['%s,osd_count'%profile_name]
-    ret = re.search('osdmap .*: (\d+) osds: (\d+) up, (\d+) in',output)
-    if ret and ( int(ret.group(1)) == int(exp_osd_map_count) ) and ( int(ret.group(2)) == int(exp_osd_map_count) ) and ( int(ret.group(1)) == int(exp_osd_map_count) ) :
-       msg = "INFO: ceph status is PASS and osd count is PASS and all osds are up\n"
-       print msg
-    else :
-       msg = "ERROR: ceph status is NOT OK, osdmap count does not match"
-       print msg
-       test_obj.argument['err_msg'] += msg
-       sys.exit(1)
+       if re.search('mons down',output):
+          msg = "ERROR: some mons process is down."
+          print msg
+          test_obj.argument['err_msg'] += msg
+          sys.exit(1)
+          
+       if re.search('stuck|degraded|unclean|undersized',output):
+          msg = "ERROR: ceph status has issue."
+          print msg
+          test_obj.argument['err_msg'] += msg
+          sys.exit(1)
+          
+       out = re.search('mons at.*',output).group()
+       ret = re.findall('=(.*?):',out)
+       ret.sort()
+       expected_mon_list = testbed_config['%s,mon_ip'%profile_name]
+       expected_mon_list.sort()
+       print ret,expected_mon_list
+       if ret == expected_mon_list :
+           print "INFO: mon IP is correct"
+       else:
+           print "ERROR: mon IP list is not correct"
+           test_obj.argument['err_msg'] += "ERROR: mon IP list is not correct"
+           sys.exit(1)
+
+       exp_osd_map_count = testbed_config['%s,osd_count'%profile_name]
+       ret = re.search('osdmap .*: (\d+) osds: (\d+) up, (\d+) in',output)
+       if ret and ( int(ret.group(1)) == int(exp_osd_map_count) ) and ( int(ret.group(2)) == int(exp_osd_map_count) ) and ( int(ret.group(1)) == int(exp_osd_map_count) ) :
+          msg = "INFO: ceph status is PASS and osd count is PASS and all osds are up\n"
+          print msg
+       else :
+          msg = "ERROR: ceph status is NOT OK, osdmap count does not match"
+          print msg
+          test_obj.argument['err_msg'] += msg
+          sys.exit(1)
 
     print "CEPH_STATUS_CHECK_OK"
 
