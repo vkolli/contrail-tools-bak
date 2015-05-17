f550ae36ba9121a9866b3504fbd40e218d5946c8
Vageesan Thanikachalam <vageesant@juniper.net>
2015-05-16 23:40:27 -0700

    included ceph_profile in the execution folder

--- testers/run_ceph_sanity.sh
+++ testers/run_ceph_sanity.sh
@@ -7,7 +7,8 @@ testbeds=(${AVAILABLE_TESTBEDS//,/ })
 echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
 
 return_val=`exec_cmds -s $TASK_RUNNER_HOST_STRING -p $TASK_RUNNER_HOST_PASSWORD -c "
-    cd /tmp/$AVAILABLE_TESTBEDS/;
+    mkdir -p /tmp/$AVAILABLE_TESTBEDS/$CEPH_PROFILE;
+    cd /tmp/$AVAILABLE_TESTBEDS/$CEPH_PROFILE;
     rm -rf contrail-tools;
     git clone git@github.com:Juniper/contrail-tools.git;
     cd contrail-tools/testers/storage_scripts;
