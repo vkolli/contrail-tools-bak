f92051fe2cad76aa8d3401e322a72181e006db12
Vageesan Thanikachalam <vageesant@juniper.net>
2015-05-21 22:47:57 -0700

    included unlocking testbed on die function. included sanity function call in fab script and SM script

--- testers/basic_job_ceph.sh
+++ testers/basic_job_ceph.sh
@@ -9,5 +9,23 @@ echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
 
 get_testbed
 run_ceph_fab_sanity
-sh run_ceph_sanity.sh
+
+return_val=`exec_cmds -s $TASK_RUNNER_HOST_STRING -p $TASK_RUNNER_HOST_PASSWORD -c "
+    mkdir -p /tmp/$AVAILABLE_TESTBEDS/$CEPH_PROFILE;
+    cd /tmp/$AVAILABLE_TESTBEDS/$CEPH_PROFILE;
+    rm -rf contrail-tools;
+    git clone git@github.com:Juniper/contrail-tools.git;
+    cd contrail-tools/testers/storage_scripts;
+    python execute_ceph_suite.py $AVAILABLE_TESTBEDS $CEPH_PROFILE $INSTALL_MODE;
+  "`
+
+flag=`awk 'BEGIN{match_found=0}{if ($0 ~ /CEPH_SANITY_PASS/) match_found=1;}END{print match_found}' <<< $return_val`
+
+if [ $flag -gt 0 ];then
+    echo "CEPH_SANITY : PASS"
+else
+    die "CEPH_SANITY : FAILED"
+fi
+
+
 unlock_testbed $TBFILE_NAME
