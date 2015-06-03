b4a656a4d71921e4fbb53459438069d1809cea58
Vageesan Thanikachalam <vageesant@juniper.net>
2015-06-03 00:39:18 -0700

    fixed ha config and bond interface config

--- testbeds/testbed_soln1.py
+++ testbeds/testbed_soln1.py
@@ -1,62 +1,63 @@
-from fabric.api import env
+from fabric.api import env                                           
 
 #Management ip addresses of hosts in the cluster
-host1 = 'root@172.16.70.2'
-host2 = 'root@172.16.70.3'
-host3 = 'root@172.16.70.4'
-host4 = 'root@172.16.70.7'
-host5 = 'root@172.16.70.8'
-host6 = 'root@172.16.70.9'
-host7 = 'root@172.16.70.10'
-host8 = 'root@172.16.70.11'
-host9 = 'root@172.16.70.12'
+host1 = 'root@172.16.70.2'                      
+host2 = 'root@172.16.70.3'                      
+host3 = 'root@172.16.70.4'                      
+host4 = 'root@172.16.70.7'                      
+host5 = 'root@172.16.70.8'                      
+host6 = 'root@172.16.70.9'                      
+host7 = 'root@172.16.70.10'                     
+host8 = 'root@172.16.70.11'                     
+host9 = 'root@172.16.70.12'                     
 
 #External routers if any
-ext_routers = [('montreal', '10.87.140.185')]
+#ext_routers = [('montreal', '10.87.140.185')]
+ext_routers = [('sydney', '172.16.80.100'), ('tasman', '172.16.80.200')]
 
 #Autonomous system number
-router_asn = 64512
+router_asn = 64512       
 
 #Host from which the fab commands are triggered to install and provision
-host_build = 'root@172.16.70.2'
+host_build = 'root@172.16.70.2'                                         
 
 #Role definition of the hosts.
-env.roledefs = {
+env.roledefs = {              
     'all': [host1, host2, host3,host4,host5,host6,host7,host8,host9],
-    'cfgm': [host1,host2,host3],
-    'openstack': [host1,host2,host3],
-    'control': [host1,host2,host3],
-    'compute': [host4, host5,host6,host7,host8,host9],
-    'collector': [host1,host2,host3],
-    'webui': [host1],
-    'database': [host1,host2,host3],
-    'build': [host_build],
-}
+    'cfgm': [host1,host2,host3],                                     
+    'openstack': [host1,host2,host3],                                            
+    'control': [host1,host2,host3],                                  
+    'compute': [host4, host5,host6,host7,host8,host9],               
+    'collector': [host1,host2,host3],                                
+    'webui': [host1,host2,host3],                                                
+    'database': [host1,host2,host3],                                 
+    'build': [host_build],                                           
+}                                                                    
 
 env.hostnames = {
     'all': ['csol1-node2','csol1-node3','csol1-node4','csol1-node7','csol1-node8','csol1-node9','csol1-node10','csol1-node11','csol1-node12']
-}
+}                                                                                                                                            
 
 #Openstack admin password
 env.openstack_admin_password = 'c0ntrail123'
 
 env.password = 'c0ntrail123'
-#Passwords of each host
-env.passwords = {
-    host1: 'c0ntrail123',
-    host2: 'c0ntrail123',
-    host3: 'c0ntrail123',
-    host4: 'c0ntrail123',
-    host5: 'c0ntrail123',
-    host6: 'c0ntrail123',
-    host7: 'c0ntrail123',
-    host8: 'c0ntrail123',
-    host9: 'c0ntrail123',
+#Passwords of each host     
+env.passwords = {           
+    host1: 'c0ntrail123',   
+    host2: 'c0ntrail123',   
+    host3: 'c0ntrail123',   
+    host4: 'c0ntrail123',   
+    host5: 'c0ntrail123',   
+    host6: 'c0ntrail123',   
+    host7: 'c0ntrail123',   
+    host8: 'c0ntrail123',   
+    host9: 'c0ntrail123',   
     host_build: 'c0ntrail123',
-}
+}                             
 
 #For reimage purpose
-env.ostypes = {
+env.ostypes = {     
     host1: 'ubuntu',
     host2: 'ubuntu',
     host3: 'ubuntu',
@@ -66,27 +67,33 @@ env.ostypes = {
     host7: 'ubuntu',
     host8: 'ubuntu',
     host9: 'ubuntu',
-}
+}                   
 
 #OPTIONAL BONDING CONFIGURATION
 #==============================
-#Inferface Bonding
+#Inferface Bonding             
 #OPTIONAL BONDING CONFIGURATION
 #==============================
-#Inferface Bonding
-#bond= {
-#    host2 : { 'name': 'bond0', 'member': ['p2p0p0','p2p0p1','p2p0p2','p2p0p3'], 'mode':'balance-xor' },
-#    host5 : { 'name': 'bond0', 'member': ['p4p0p0','p4p0p1','p4p0p2','p4p0p3'], 'mode':'balance-xor' },
-#}
-
+#Inferface Bonding             
+bond= {                        
+    host1 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host2 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host3 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host4 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host5 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host6 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host7 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host8 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	host9 : { 'name': 'bond0', 'member': ['eth2','eth3'], 'mode':'802.3ad' }, 
+	}
 #OPTIONAL SEPARATION OF MANAGEMENT AND CONTROL + DATA
 #====================================================
-#Control Interface
-#control = {
+#Control Interface                                   
+#control = {                                         
 #    host1 : { 'ip': '10.87.140.197/22', 'gw' : '10.87.159.254', 'device':'eth0' },
 #    host2 : { 'ip': '10.87.140.198/22', 'gw' : '10.87.159.254', 'device':'eth0' },
 #    host3 : { 'ip': '10.87.140.199/22', 'gw' : '10.87.159.254', 'device':'eth0' },
-#}
+#}                                                                                 
 
 #Data Interface
 control_data = {
@@ -108,7 +115,7 @@ env.interface_rename = False
 #service_token = 'your_token'
 
 #Specify keystone IP
-#keystone_ip = '1.1.1.1'
+#keystone_ip = '172.16.70.2'
 
 #Specify Keystone admin user if not same as  admin
 #keystone_admin_user = 'nonadmin'
@@ -133,3 +140,10 @@ env.interface_rename = False
 
 #Ceph related
 
+# HA Config
+
+env.ha = {
+    'internal_vip' : '172.16.80.25',
+    'external_vip' : '172.16.70.25',
+
+}
