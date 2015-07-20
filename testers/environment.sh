64849b58d3965f5cbbc2cd8236cf8aad0a15f652
Sudheendra Rao <sudheendra@juniper.net>
2015-07-19 21:38:22 -0700

    removing redundant sourcing of environment.sh and handling 14.04.1 sanity.

--- testers/environment.sh
+++ testers/environment.sh
@@ -24,8 +24,6 @@ export API_SERVER_HOST_USER=`echo $API_SERVER_HOST_STRING | cut -d @ -f1`
 export API_SERVER_HOST_PASSWORD=${API_SERVER_HOST_PASSWORD:-c0ntrail123}
 #-------
 
-#declare -i IS_UBUNTU_14042
-
 export SKIP_REIMAGE=${SKIP_REIMAGE:-0}
 export SKIP_BRINGUP=${SKIP_BRINGUP:-0}
 export SKIP_ZONES=${SKIP_ZONES:-0}
@@ -74,14 +72,6 @@ if [ ${BRANCH} == "mainline" ]; then
     FAB_GIT_BRANCH='master'
 fi
 
-declare -A BUILD_MAP
-BUILD_MAP=( [ubuntu-12-04]=ubuntu-12.04.3 [centos64_os]=centos-6.4 [centos65]=centos-6.5 [ubuntu-14-04]=ubuntu-14.04 [redhat70]=redhat-7.0 [centos71]=centos-7.1 )
-
-if [ $IS_UBUNTU_14042 -eq 1 ]; then
-    export REIMAGE_PARAM="ubuntu-14.04.2"
-else
-    export REIMAGE_PARAM=${BUILD_MAP[${DISTRO}]}
-fi
 lsb_dist=''
 
 ###########
