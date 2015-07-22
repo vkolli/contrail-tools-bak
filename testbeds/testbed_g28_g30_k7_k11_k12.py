d20fac82ba7025a9dd257118b3349bb658970c8a
Alok Kumar <kalok@juniper.net>
2015-07-22 00:04:36 -0700

    converting to multi-interface

--- testbeds/testbed_g28_g30_k7_k11_k12.py
+++ testbeds/testbed_g28_g30_k7_k11_k12.py
@@ -7,7 +7,7 @@ host4 = 'root@10.204.216.227'
 host5 = 'root@10.204.216.231'
 host6 = 'root@10.204.216.232'
 
-ext_routers = [('hooper','10.204.217.240')]
+ext_routers = [('hooper','192.168.196.10')]
 router_asn = 64512
 public_vn_rtgt = 2226
 public_vn_subnet = '10.204.221.144/28'
@@ -16,12 +16,12 @@ host_build = 'stack@10.204.216.49'
 
 env.roledefs = {
     'all': [host1, host2, host3, host4, host5, host6],
-    'cfgm': [host1, host2],
-    'webui': [host1],
-    'openstack': [host1],
-    'control': [host2, host3],
-    'collector': [host1],
-    'database': [host1],
+    'cfgm': [host1, host3],
+    'webui': [host2],
+    'openstack': [host3],
+    'control': [host1, host3],
+    'collector': [host1, host3],
+    'database': [host1, host2, host3],
     'compute': [host4, host5, host6],
     'build': [host_build]
 }
@@ -51,6 +51,15 @@ env.ostypes = {
     host6:'ubuntu',
 }
 
+control_data = {
+    host1 : { 'ip': '192.168.196.1/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+    host2 : { 'ip': '192.168.196.2/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+    host3 : { 'ip': '192.168.196.3/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+    host4 : { 'ip': '192.168.196.4/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+    host5 : { 'ip': '192.168.196.5/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+    host6 : { 'ip': '192.168.196.6/24', 'gw' : '192.168.196.254', 'device':'eth1' },
+}
+
 minimum_diskGB=32
 env.test_repo_dir='/home/stack/multi-node/ubuntu/icehouse/contrail-test'
 env.mail_from='contrail-build@juniper.net'
@@ -58,4 +67,4 @@ env.mail_to='dl-contrail-sw@juniper.net'
 env.interface_rename = True
 multi_tenancy=True
 env.encap_priority =  "'VXLAN','MPLSoUDP','MPLSoGRE'"
-env.log_scenario='MultiNode single interface Sanity'
+env.log_scenario='MultiNode Multi-Interface Sanity'
