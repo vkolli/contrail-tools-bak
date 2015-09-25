dba9cf966d943c3724d7a9d140fc0508a256670b
Ritam Gangopadhyay <ritam@juniper.net>
2015-09-25 10:52:58 +0530

    Adding new SM regression job.

---
+++ testers/smgr_regression_job.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+TOOLS_WS=${TOOLS_WS:-$(pwd)}
+source $TOOLS_WS/testers/utils
+source $TOOLS_WS/testers/smgr_utils
+# AVAILABLE_TESTBEDS is a comma separated list of testbed filenames or paths
+testbeds=(${AVAILABLE_TESTBEDS//,/ })
+echo "AVAILABLE TESTBEDS : ${testbeds[@]}"
+
+get_testbed
+run_smgr_regression_task
+unlock_testbed $TBFILE_NAME
