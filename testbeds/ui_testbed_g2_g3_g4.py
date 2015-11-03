c8a59f42caf5f09262b0089839ea3eef893c2e0c
Ganesha H V <ganeshahv@juniper.net>
2015-11-03 15:06:28 +0530

    Adding 4th node to the UI testbed

--- testbeds/ui_testbed_g2_g3_g4.py
+++ testbeds/ui_testbed_g2_g3_g4.py
@@ -3,7 +3,7 @@ from fabric.api import env
 host1 = 'root@10.204.217.42'
 host2 = 'root@10.204.217.43'
 host3 = 'root@10.204.217.44'
-
+host4 = 'root@10.204.217.47'
 ext_routers = []
 router_asn = 64512
 
@@ -11,19 +11,19 @@ host_build = 'stack@10.204.216.49'
 
 
 env.roledefs = {
-    'all': [host1, host2, host3],
+    'all': [host1, host2, host3i, host4],
     'cfgm': [host1],
     'webui': [host1, host2, host3],
     'openstack': [host2],
     'control': [host1, host2, host3],
     'collector': [host1, host2, host3],
     'database': [host1],
-    'compute': [host1, host2, host3],
+    'compute': [host1, host2, host3, host4],
     'build': [host_build]
 }
 
 env.hostnames = {
-    'all': ['nodeg2', 'nodeg3', 'nodeg4']
+    'all': ['nodeg2', 'nodeg3', 'nodeg4', 'nodeg7']
 }
 
 env.openstack_admin_password = 'contrail123'
@@ -32,6 +32,7 @@ env.passwords = {
     host1: 'c0ntrail123',
     host2: 'c0ntrail123',
     host3: 'c0ntrail123',
+    host4: 'c0ntrail123',
     host_build: 'stack@123',
 }
 
@@ -39,6 +40,7 @@ env.ostypes = {
     host1:'ubuntu',
     host2:'ubuntu',
     host3:'ubuntu',
+    host4:'ubuntu',
 }
 minimum_diskGB=32
 multi_tenancy=True
