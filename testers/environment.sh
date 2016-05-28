578bae26ee46d96965eacd87f456347bcf851af0
Shaju V K <shajuvk@juniper.net>
2016-05-27 18:08:01 -0700

    changes to run vcenter-orchestartor-sanity

--- testers/environment.sh
+++ testers/environment.sh
@@ -25,8 +25,6 @@ fi
 export TEST_RUN=${TEST_RUN:-'contrail-test'}
 export TEST_CONTAINER_IMAGE=${TEST_CONTAINER_IMAGE:-''}
 export TEST_CONTAINER_IMAGE_DIR=${TEST_CONTAINER_IMAGE_DIR:-"/github-build/${BRANCH}/${BUILDID}/ubuntu-14-04/${SKU}/artifacts/"}
-
-
 # If BRANCH, BUILID, DISTRO, SKU are not defined,
 # PKG_FILE path needs to be set
 export PKG_FILE
