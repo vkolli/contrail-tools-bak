4787a095ae5dce29e73f18069b34464f87aec102
Shaju V K <shajuvk@juniper.net>
2016-06-10 11:48:46 -0700

    removed loops

--- testers/environment.sh
+++ testers/environment.sh
@@ -10,8 +10,11 @@ export TOOLS_WS=${TOOLS_WS:-$PWD}
 export BRANCH=${BRANCH:-mainline}
 export BUILDID=${BUILDID:-LATEST}
 export DISTRO=${DISTRO:-"ubuntu-12-04"}
-export SKU=${SKU:-icehouse}
-export VCENTER_TEST_SKU=${VCENTER_TEST_SKU:-kilo}
+if [  $VCENTER_ONLY_TESTBED -eq 1 ]; then
+    export TEST_SKU=${VCENTER_TEST_SKU:-kilo}
+else
+    export TEST_SKU=${SKU:-icehouse}
+fi
 ##
 # TEST_RUN_INFRA: to specify where the test is run, possible values are
 # docker: using docker container for test run
@@ -25,11 +28,7 @@ fi
 export TEST_RUN=${TEST_RUN:-'contrail-test'}
 export TEST_CONTAINER_IMAGE=${TEST_CONTAINER_IMAGE:-''}
 
-if [  $VCENTER_ONLY_TESTBED -eq 1 ]; then
-    export TEST_CONTAINER_IMAGE_DIR=${TEST_CONTAINER_IMAGE_DIR:-"/github-build/${BRANCH}/${BUILDID}/ubuntu-14-04/${VCENTER_TEST_SKU}/artifacts/"}
-else
-    export TEST_CONTAINER_IMAGE_DIR=${TEST_CONTAINER_IMAGE_DIR:-"/github-build/${BRANCH}/${BUILDID}/ubuntu-14-04/${SKU}/artifacts/"}
-fi
+export TEST_CONTAINER_IMAGE_DIR=${TEST_CONTAINER_IMAGE_DIR:-"/github-build/${BRANCH}/${BUILDID}/ubuntu-14-04/${TEST_SKU}/artifacts/"}
 # If BRANCH, BUILID, DISTRO, SKU are not defined,
 # PKG_FILE path needs to be set
 export PKG_FILE
