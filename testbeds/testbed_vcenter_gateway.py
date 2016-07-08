ed31c6e99de343665b7f7661c5744cbc7f25f675
Sandip Dey <sandipd@juniper.net>
2016-07-08 01:42:13 -0700

    vcenter gateway testbed

--- testbeds/testbed_vcenter_gateway.py
+++ testbeds/testbed_vcenter_gateway.py
@@ -4,6 +4,7 @@ from fabric.api import env
 host1 = 'root@10.204.216.15'
 host2 = 'root@10.204.216.10'
 host3 = 'root@10.204.217.209'
+host4 = 'root@10.204.217.122'
 
 
 ext_routers = [('blr-mx1', '10.204.216.253')]
@@ -15,19 +16,19 @@ host_build = 'stack@10.204.216.49'
 #host_build = 'root@10.204.216.7'
 
 env.roledefs = {
-    'all': [ host1, host2,host3],
+    'all': [ host1, host2,host3,host4],
     'cfgm': [host1],
     'openstack': [host1],
     'webui': [host1],
     'control': [host1],
     'collector': [host1],
     'database': [host1],
-    'compute': [host2,host3],
+    'compute': [host2,host3,host4],
     'build': [host_build]
 }
 
 env.hostnames = {
-    'all': ['nodea19', 'nodea14','nodel3']
+    'all': ['nodea19', 'nodea14','nodel3','nodei10']
 }
 
 env.password = 'c0ntrail123'
@@ -35,6 +36,7 @@ env.passwords = {
     host1: 'c0ntrail123',
     host2: 'c0ntrail123',
     host3: 'c0ntrail123',
+    host4: 'c0ntrail123',
     host_build: 'stack@123',
 }
 
@@ -42,6 +44,7 @@ env.ostypes = {
     host1:'ubuntu',
     host2:'ubuntu',
     host3:'ubuntu',
+    host4:'ubuntu',
 }
 
 #env.orchestrator = 'vcenter'
@@ -49,10 +52,10 @@ control_data = {
     host1 : { 'ip': '192.168.250.4/24', 'gw' : '192.168.250.254', 'device':'em1' },
     host2 : { 'ip': '192.168.250.5/24', 'gw' : '192.168.250.254', 'device':'em1' },
     host3 : { 'ip': '192.168.250.8/24', 'gw' : '192.168.250.254', 'device':'em2' },
+    host4 : { 'ip': '192.168.250.19/24', 'gw' : '192.168.250.254', 'device':'p6p2' },
 }
 
 minimum_diskGB=32
-env.test_repo_dir='/root/contrail-test'
 env.mail_from='contrail-build@juniper.net'
 env.mail_to='sandipd@juniper.net'
 multi_tenancy=True
@@ -66,3 +69,4 @@ env.ntp_server = 'ntp.juniper.net'
 #enable ceilometer
 #enable_ceilometer = True
 #ceilometer_polling_interval = 60
+env.test_repo_dir='/root/contrail-test'
