9d1d4e89bc200c13839e6f8a97af6e57d40f6ee9
Jeba Paulaiyan <jebap@juniper.net>
2016-09-13 16:43:52 -0700

    Split db and cfgm roles for hasanity

--- testbeds/testbed_sanity_a5d11e09.py
+++ testbeds/testbed_sanity_a5d11e09.py
@@ -2,12 +2,17 @@ from fabric.api import env
 import os
 
 #Management ip addresses of hosts in the cluster
-host1 = 'root@10.87.143.84'
-host2 = 'root@10.87.143.90'
-host3 = 'root@10.87.143.92'
-host4 = 'root@10.87.143.94'
-#host5 = 'root@10.87.143.98'
-host6 = 'root@10.87.143.100'
+host1 = 'root@10.87.143.10'
+host2 = 'root@10.87.143.12'
+host3 = 'root@10.87.143.16'
+host4 = 'root@10.87.143.18'
+host5 = 'root@10.87.143.35'
+host6 = 'root@10.87.143.37'
+host7 = 'root@10.87.143.90'
+host8 = 'root@10.87.143.92'
+host9 = 'root@10.87.143.94'
+host10 = 'root@10.87.143.84'
+host11 = 'root@10.87.143.100'
 
 #External routers if any
 ext_routers = [('montreal', '10.87.140.140')]
@@ -20,19 +25,19 @@ host_build = 'root@10.84.24.64'
 
 #Role definition of the hosts.
 env.roledefs = {
-    'all': [host1, host2, host3, host4, host6 ],
-    'cfgm': [host2, host3, host4],
-    'openstack': [host2, host3, host4],
-    'control': [host2, host3, host4],
-    'compute': [host1, host6],
-    'collector': [host2, host3, host4],
-    'webui': [host2, host3, host4],
-    'database': [host2, host3, host4],
+    'all': [host1, host2, host3, host4, host5, host6, host7, host8, host9, host10, host11],
+    'cfgm': [host4, host5, host6],
+    'openstack': [host1, host2, host3],
+    'control': [host4, host5, host6],
+    'compute': [host10, host11],
+    'collector': [host4, host5, host6],
+    'webui': [host4, host5, host6],
+    'database': [host7, host8, host9],
     'build': [host_build],
 }
 
 env.hostnames = {
-    'all': ['a5d11e35','a5d11e14','a5d11e13','a5d11e12','a5d11e09']
+    'all': ['a5d11e42','a5d06e38','a5d10e33','a5d10e34','a5d11e37','a5d11e38','a5d11e14','a5d11e13','a5d11e12','a5d11e35','a5d11e09']
 }
 
 #Openstack admin password
@@ -56,7 +61,13 @@ env.passwords = {
     host2: 'c0ntrail123',
     host3: 'c0ntrail123',
     host4: 'c0ntrail123',
+    host5: 'c0ntrail123',
     host6: 'c0ntrail123',
+    host7: 'c0ntrail123',
+    host8: 'c0ntrail123',
+    host9: 'c0ntrail123',
+    host10: 'c0ntrail123',
+    host11: 'c0ntrail123',
     host_build: 'c0ntrail123',
 }
 
@@ -66,7 +77,13 @@ env.ostypes = {
     host2: 'ubuntu',
     host3: 'ubuntu',
     host4: 'ubuntu',
+    host5: 'ubuntu',
     host6: 'ubuntu',
+    host7: 'ubuntu',
+    host8: 'ubuntu',
+    host9: 'ubuntu',
+    host10: 'ubuntu',
+    host11: 'ubuntu',
 }
 
 #OPTIONAL BONDING CONFIGURATION
@@ -91,11 +108,17 @@ env.ostypes = {
 
 #Data Interface
 control_data = {
-   host1 : { 'ip': '192.168.11.1/24', 'gw' : '192.168.11.100', 'device':'p51p2' },
-   host2 : { 'ip': '192.168.11.2/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
-   host3 : { 'ip': '192.168.11.3/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
-   host4 : { 'ip': '192.168.11.4/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
-   host6 : { 'ip': '192.168.11.6/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
+   host1 : { 'ip': '192.168.11.1/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host2 : { 'ip': '192.168.11.2/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host3 : { 'ip': '192.168.11.3/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host4 : { 'ip': '192.168.11.4/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host5 : { 'ip': '192.168.11.5/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host6 : { 'ip': '192.168.11.6/24', 'gw' : '192.168.11.100', 'device':'p5p2' },
+   host7 : { 'ip': '192.168.11.7/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
+   host8 : { 'ip': '192.168.11.8/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
+   host9 : { 'ip': '192.168.11.9/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
+   host10 : { 'ip': '192.168.11.10/24', 'gw' : '192.168.11.100', 'device':'p51p2' },
+   host11 : { 'ip': '192.168.11.11/24', 'gw' : '192.168.11.100', 'device':'p51p1' },
 }
 
 #To disable installing contrail interface rename package
@@ -140,6 +163,10 @@ env.ha = {
              'internal_vip' : '192.168.11.50',
 	     'internal_virtual_router_id' : 111,
 	     'external_virtual_router_id' : 112,
+             'contrail_external_vip' : '10.87.143.151',
+             'contrail_internal_vip' : '192.168.11.51',
+	     'contrail_internal_virtual_router_id' : 113,
+	     'contrail_external_virtual_router_id' : 114,
          }
 ipmi_username = 'root'
 ipmi_password = 'superuser'
@@ -149,6 +176,12 @@ env.hosts_ipmi = {
     '10.87.143.92': '10.87.143.91',
     '10.87.143.94': '10.87.143.93',
     '10.87.143.100': '10.87.143.99',
+    '10.87.143.10': '10.87.143.9',
+    '10.87.143.12': '10.87.143.11',
+    '10.87.143.16': '10.87.143.15',
+    '10.87.143.18': '10.87.143.17',
+    '10.87.143.35': '10.87.143.34',
+    '10.87.143.37': '10.87.143.36',
 }
 # HA Test configuration
 ha_setup = 'True'
